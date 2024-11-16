dd1718231c2f6012b124b761f20d09ba3afa2abf
daniel <danielsussa@gmail.com>
2024-11-16 12:06:25 -0300

    tmp

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,20 @@
-import {greet} from "./helper";
+import {getCursorColumn, getCursorPosition, greet, restoreCursorPosition, textToTextMap} from "./helper";
 import './styles/styles.scss';
 import './styles/color.scss';
 import $ from 'jquery';
-window.$ = window.jQuery = jQuery;
-import {Controller} from "./controller";
+import {Render} from "./render";
 
-const controller = new Controller()
+const textMaps = textToTextMap('## Hello world\nprevious point mean that Chrome could both speak and listen on Desktop, but I haven’t tried the non OSS version (yet) … update: I did and it worked well!')
+const render = new Render(textMaps)
 
-console.log(greet('Daniel'))
 
 $(() => {
-  controller.showControl()
-  const span = document.createElement("span")
-    span.innerText = greet('Daniel')
-    $("body").append(span)
+
+  render.start()
+
+  $('.editable').on('keyup click', function () {
+    const cursor = getCursorColumn()
+    const element = render.updateCursor(cursor)
+     restoreCursorPosition(element, {startOffset: cursor})
+  });
 })
