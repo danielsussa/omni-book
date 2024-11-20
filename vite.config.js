bcc20b7c928efb997c9c8a94b20cfe0999f69e38
daniel <danielsussa@gmail.com>
2024-11-20 14:16:16 -0300

    fix docs

--- vite.config.js
+++ vite.config.js
@@ -2,21 +2,33 @@ import { defineConfig } from 'vite';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
+    base: './', // Use relative paths
     build: {
         outDir: 'dist', // Specify the output directory
+        rollupOptions: {
+            input: {
+                main: './index.html', // Entry HTML file
+                nested: './src/app.ts', // Specific TypeScript entry
+            },
+            output: {
+                entryFileNames: 'assets/[name].js',
+                chunkFileNames: 'assets/[name]-[hash].js',
+                assetFileNames: 'assets/[name]-[hash][extname]',
+            },
+        },
     },
     define: {
         $: 'window.jQuery',
         jQuery: 'window.jQuery',
     },
-    plugins: [
-        viteStaticCopy({
-            targets: [
-                {
-                    src: 'index.html', // Source file
-                    dest: ''           // Destination folder in `dist` (root of dist in this case)
-                }
-            ]
-        })
-    ]
+    // plugins: [
+    //     viteStaticCopy({
+    //         targets: [
+    //             {
+    //                 src: 'index.html', // Source file
+    //                 dest: ''           // Destination folder in `dist` (root of dist in this case)
+    //             }
+    //         ]
+    //     })
+    // ]
 });
